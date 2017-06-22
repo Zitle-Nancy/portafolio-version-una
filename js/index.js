ed40b8a077b22d9b4660d1706c068c0af0d8d3d4
nancyzj <ancizj@hotmail.com>
2017-06-22 10:12:14 -0500

    version una en habilidades y sobre mi

--- js/index.js
+++ js/index.js
@@ -1,30 +1,53 @@
+
 (function() {
+
+	page('/',index);
+	page('/habilidades', habilidades);
+
+	page();
+
+	function index(){
+		$(".contenido").addClass("hidden");
+		$('#sobre-mi').removeClass("hidden");
+		$("#myNav").removeClass('nav-visible');
+	}
+
+	function habilidades() {
+		$(".contenido").addClass("hidden");
+		$('#seccion-habilidades').removeClass('hidden');
+		$("#myNav").removeClass('nav-visible');
+		//barras
+		$('.skillbar').each(function(){
+			$(this).find('.skillbar-bar').animate({
+				width:$(this).attr('data-percent')
+			},1000);
+		});
+	}
 
 	var efectoNombre = {
 		strings: ["Nancy Zitle Juárez"],
 		typeSpeed: 100,
 		loop: true
 	}
-
 	var cargarPagina = function(){
 		$('#menu-comprimido').click(openNav);
 		$('#closeNav').click(closeNav);
 		//typed es un metodo de un objeto de jquery
 		$('.element').typed(efectoNombre);
-		//barras
-		$('.skillbar').each(function(){
-			$(this).find('.skillbar-bar').animate({
-				width:$(this).attr('data-percent')
-			},6000);
-		});
+		
+		// cambiar rutas
+		// page('/habilidades',habilidades)
+		// page('/trabajos', trabajos)
+		// page('/contactos',contactos)
+		// fin de rutas
 	}
 
 	var openNav = function() {
-    	document.getElementById("myNav").style.width = "100%";
+		// revisar
+		$("#myNav").addClass('nav-visible');
 	}
-
 	var closeNav = function(){
-		document.getElementById("myNav").style.width = "0%";
+		$("#myNav").removeClass('nav-visible');
 	}
 
 	
