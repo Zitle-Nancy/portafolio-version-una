e77d3d07b8566769be8a769de743c8d24b5da581
nancyzj <ancizj@hotmail.com>
2017-06-22 05:19:38 -0500

    mejoras

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
 (function() {
+
+	var efectoNombre = {
+		strings: ["Nancy Zitle Juárez"],
+		typeSpeed: 100,
+		loop: true
+	}
+
 	var cargarPagina = function(){
 		$('#menu-comprimido').click(openNav);
 		$('#closeNav').click(closeNav);
+		//typed es un metodo de un objeto de jquery
+		$('.element').typed(efectoNombre);
 	}
+
 	var openNav = function() {
     	document.getElementById("myNav").style.width = "100%";
 	}
